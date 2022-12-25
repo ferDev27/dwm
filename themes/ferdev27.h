97149e124c87d69ddca85148e41ad752c07dfafc
Fernando Martin <fermartingmr@gmail.com>
2022-12-25 23:32:39 +0100

    tweak cs

--- themes/ferdev27.h
+++ themes/ferdev27.h
@@ -4,15 +4,13 @@
 static const char col_barbg[]		= "#0f101a"; 
 
 //Tag square bg
-//static const char col_tagbg[]		= "#1e1f33";  
-static const char col_tagbg[]		= "#a151d3";  
+static const char col_tagbg[]		= "#7e4dcc";  
 
 // Window border
 static const char col_winSel[]		= "#7a5ccc";
 static const char col_winUnSel[]	= "#1a1b26";
 
 // tag icons/numbers fg(selected) and bg(empty)
-//static const char col_wsfg[]		= "#ecf0c1"; 
 static const char col_wsfg[]		= "#f1ffff"; 
 static const char col_wsbg[]		= "#373b52"; 
 
@@ -20,5 +18,5 @@ static const char col_wsbg[]		= "#373b52";
 static const char col_occ[]         = "#f1ffff"; 
 
 // title
-static const char col_tifg[]         = "#a151d3"; 
+static const char col_tifg[]         = "#7e4dcc"; 
 static const char col_tibg[]         = "#0f101a"; 
